e8d77a85d6ca915d6cdc2820ac5b00937c9c6757
Khumo Ratsoana <166295436+KhumoRatsoana@users.noreply.github.com>
2025-03-13 14:06:44 +0200

    Add project files.

---
+++ ICE_TASK_THREE/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ICE_TASK_THREE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
